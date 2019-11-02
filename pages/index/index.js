f83600c370b6c1cd6151b75e6718b002ae24124c
XueHaiLan <1320875485@qq.com>
2019-11-02 22:44:43 +0800

    第一次上传

---
+++ pages/index/index.js
@@ -0,0 +1,145 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    small:[
+      {jiuye:"https://m.boxuegu.com/web/images/index/course.png",txt:"就业课"},
+      {jiuye:"https://m.boxuegu.com/web/images/index/mircoCourse.png",txt:"提升课"},
+      {jiuye:"https://m.boxuegu.com/web/images/index/freeMicroCourse.png",txt:"免费课"},
+      {jiuye:"https://m.boxuegu.com/web/images/index/courseType.png",txt:"课程分类"}
+      
+    ],
+    jiuye:[
+      {
+        pic:"https://file.boxuegu.com/f6c878722f394a7c9bd12c0319f00037.png",
+        top:"JavaEE",
+        center:"JavaEE是一个开放的平台、基于标准的平台",
+        button1:"1784人学习",
+        button2:"￥14980.00"
+      },
+      {
+        pic:"https://file.boxuegu.com/e19c53c6c1fa468aad6e11cb61d81b9a.png",
+        top:"Python+人工智能",
+        center:"最有前景的开发职位",
+        button1:"877人学习",
+        button2:"￥14980.00"
+      },
+      {
+        pic:"https://file.boxuegu.com/3940be1c755a44eeb8e2e0aaf984756e.png",
+        top:"前端与移动开发",
+        center:"用户界面的构筑者",
+        button1:"1230人学习",
+        button2:"￥14980.00"
+      },
+      {
+        pic:"https://file.boxuegu.com/6c0ffe21a06d421d90be8a0061c14c1a.png",
+        top:"软件测试",
+        center:"",
+        button1:"189人学习",
+        button2:"￥12980.00"
+      }
+    ],
+    recom:[
+      {
+        pic1:"https://file.boxuegu.com/e6432db4d06c42b9aca7aede22ba9437.jpg",
+        pic2:"https://file.boxuegu.com/989b9bd2a12d471b8e7579b7182d08b6.png",
+        top1:"所有人都能学的数据分析课",
+        top2:"MySQL数据库性能优化项目实战",
+        stu1:"710人学习",
+        stu2:"107人学习",
+        man1:"￥399.00",
+        man2:"￥199.00"
+      },
+      {
+        pic1:"https://file.boxuegu.com/df25a3e7031a48d39a896d4e38d3d3d5.png",
+        pic2:"https://file.boxuegu.com/854b60de9d6742dbb29a47370b7c8650.png",
+        top1:"Java并发变成原理精讲",
+        top2:"GAM生成对抗网络入门到精通",
+        stu1:"107人学习",
+        stu2:"183人学习",
+        man1:"￥199.00",
+        man2:"￥199.00"
+      },
+      {
+        pic1:"https://file.boxuegu.com/746a8e78655a4b3ca97ccad5390fbc4b.png",
+        pic2:"https://file.boxuegu.com/90bb08a6bacd49baa5492c5eaa94c060.jpg",
+        top1:"基于KyLin搞定千亿级电信数据分析",
+        top2:"游戏AI实战课程深入浅出",
+        stu1:"187人学习",
+        stu2:"109人学习",
+        man1:"￥199.00",
+        man2:"￥199.00"
+      }
+    ],
+    free:[
+      {
+        pic1:"https://file.boxuegu.com/74773be265b840b3af9da88f6f42841d.jpg",
+        pic2:"https://file.boxuegu.com/7e1535f414a64d3fa4d60e7c8a84cca3.jpg",
+        top1:"2小时用Go语言实现人脸识别抖音",
+        top2:"JavaEE在线就业班2.0（试听课）",
+        stu1:"770人学习",
+        stu2:"107人学习",
+        man1:"免费",
+        man2:"免费"
+      },
+      {
+        pic1:"https://file.boxuegu.com/3ed09c2ae4ea4542add4ac8b098c9535.jpg",
+        pic2:"https://file.boxuegu.com/6a0330c2237b4ed3af2a2703cc2f1419.jpg",
+        top1:"Axure基础课",
+        top2:"产品经理公开课",
+        stu1:"107人学习",
+        stu2:"183人学习",
+        man1:"免费",
+        man2:"免费"
+      },
+      {
+        pic1:"https://file.boxuegu.com/ccc47386e2b048658f944d0db943321c.jpg",
+        pic2:"https://file.boxuegu.com/49b20b70e42e446cafc60f9c6fd98d84.jpg",
+        top1:"短视频创造营探店精品课程",
+        top2:"剪辑大神50招",
+        stu1:"187人学习",
+        stu2:"109人学习",
+        man1:"免费",
+        man2:"免费"
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+      
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  }
+})
